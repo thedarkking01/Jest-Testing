0343a9a4ed04dfaeb8d4c5149db6ba44152608a4
sourabh singh bais <sourabhsinghbais52@gmail.com>
2024-10-09 14:34:54 +0530

    spy implementation

---
+++ spyfunction.test.js
@@ -0,0 +1,12 @@
+test('spying on a method of an object', () => { 
+    const video={
+        play(){
+            return true;
+        },
+    };
+
+    const spy=jest.spyOn(video, 'play');
+    video.play();
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+ })
